5d2be15aff8a185fb0111892c52afc13f18f3acb
Al Samir <shahinur1591@gmail.com>
2024-06-01 20:36:42 +0600

    Font and color setup done

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#FFFFFF",
+      secondary: "#2485B0",
+      titleColor: "#000",
+      textColor: "#1F2223",
+    },
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+      serif: ["Montserrat", "serif"],
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: false,
+    darkTheme: "light",
+  },
+};
