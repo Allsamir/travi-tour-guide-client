fa325f9a9742952865f394c4f551ea08c185d684
Al Samir <shahinur1591@gmail.com>
2024-06-05 17:21:59 +0600

    jwt token creation done

--- src/provider/AuthProvider.tsx
+++ src/provider/AuthProvider.tsx
@@ -10,6 +10,7 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import { auth } from "../config/firebase.config";
+import useSecureAxios from "../hooks/useSecureAxios";
 
 interface AuthContextType {
   user: User | null;
@@ -30,6 +31,7 @@ const AuthProvider: React.FC<ChildProps> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
   const provider = new GoogleAuthProvider();
   const [loading, setLoading] = useState(true);
+  const secureAxios = useSecureAxios();
   const createUser = (email: string, password: string) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
@@ -49,11 +51,22 @@ const AuthProvider: React.FC<ChildProps> = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
       setLoading(false);
+      if (user) {
+        secureAxios
+          .post(`/users/jwt`, { email: user.email })
+          .then((res) => console.log(res.data))
+          .catch((err) => console.error(err));
+      } else {
+        secureAxios
+          .post(`/users/clearCookie`)
+          .then((res) => console.log(res.data))
+          .catch((err) => console.error(err));
+      }
     });
     return () => {
       return unsubscribe();
     };
-  }, []);
+  }, [secureAxios]);
   const authInfo = {
     user,
     loading,
